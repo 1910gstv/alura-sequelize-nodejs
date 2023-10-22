156ac2a7ed0283331916c2806e0fee0ceda9ef16
Gustavo <gustavo.hpaula@outlook.com>
2023-10-22 12:45:39 -0300

    criando seeds e populando o banco de dados

--- api/migrations/20231022150036-create-niveis.js
+++ api/migrations/20231022150036-create-niveis.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      desc - nivel: {
+      desc_nivel: {
         type: Sequelize.STRING
       },
       createdAt: {
---
+++ api/seeders/20231022153447-demo-turmas.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Turmas', [
+      {
+        data_inicio: "2020-02-01",
+        nivel_id: 1,
+        docente_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        data_inicio: "2022-06-07",
+        nivel_id: 2,
+        docente_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        data_inicio: "2021-10-05",
+        nivel_id: 3,
+        docente_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Turmas', null, {});
+
+  }
+};
